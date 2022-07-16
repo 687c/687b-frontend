a760e67af279baba949ba46bb1400797c6f70462
Jimii <jimmyimpulse2@gmail.com>
2022-07-16 16:44:41 +0300

    update: remove react wallet adapter

--- src/App.js
+++ src/App.js
@@ -4,22 +4,21 @@ import {
 	Route,
 } from "react-router-dom";
 
-
 import Nav from './components/Nav';
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Marketplace from "./pages/Marketplace";
 import Profile from "./pages/Profile";
+import Create from "./pages/Create";
 
 import './App.css';
-import { WalletProvider } from "./store";
-
+import { WalletStore } from "./store";
 
 function App() {
 
 	return (
 		<div className="App">
-			<WalletProvider>
+			<WalletStore>
 				<BrowserRouter>
 					<header className="AppHeader">
 						<Nav />
@@ -29,14 +28,23 @@ function App() {
 						<Route path="/" element={<Home />} />
 						<Route path="about" element={<About />} />
 						<Route path="marketplace" element={<Marketplace />} />
+						<Route path="create" element={<Create />} >
+							{/* <Route index element="product" /> */}
+							<Route index path="product" />
+							<Route path="nft" />
+						</Route>
+
 						<Route path="profile" element={<Profile />} />
 						<Route path="*" element={<>Nothing to see here</>} /> {/* 404 route matches nothing */}
 						{/* </Route> */}
 					</Routes>
 				</BrowserRouter>
-			</WalletProvider>
+			</WalletStore>
 		</div>
 	);
 }
 
 export default App;
+
+
+// the first e-commerce site built on top of solana
